89da29daa551bfd3e2961a8c015dbb647cb75a23
artsok <relix@mail.ru>
2018-07-23 13:59:17 +0300

    ass lesson "07. ID и Not Present"

--- avt-mob-3/src/test/kotlin/io/github/artsok/training/FirstTest.kt
+++ avt-mob-3/src/test/kotlin/io/github/artsok/training/FirstTest.kt
@@ -4,17 +4,21 @@ package io.github.artsok.training
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.openqa.selenium.By
+import org.openqa.selenium.By.id
 import org.openqa.selenium.By.xpath
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
+import ru.yandex.qatools.matchers.decorators.MatcherDecorators.should
 import ru.yandex.qatools.matchers.decorators.TimeoutWaiter.timeoutHasExpired
 import ru.yandex.qatools.matchers.decorators.WaiterMatcherDecorator.decorateMatcherWithWaiter
+import ru.yandex.qatools.matchers.webdriver.ExistsMatcher.exists
 import ru.yandex.qatools.matchers.webdriver.driver.CanFindElementMatcher.canFindElement
 import java.io.File
 import java.net.URL
@@ -40,16 +44,23 @@ class FirstTest {
     }
 
     @Test
-    fun shouldFindJavaInSearchList() {
+    fun shouldFindSpecialWordInSearchResultList() {
         actions(xpath("//*[contains(@text, 'Search Wikipedia')]"), WebElement::click, "Can't find Search Wikipedia input")
         actions(xpath("//*[contains(@text, 'Search…')]"), { element: WebElement -> element.sendKeys("Java") })
         assertThat(driver, decorateMatcherWithWaiter(
                 canFindElement(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
-                        "//*[contains(@text, 'Object-oriented programming language')]")), timeoutHasExpired(5000)))
+                        "//*[contains(@text, 'Object-oriented programming language')]")), timeoutHasExpired(5000L)))
+    }
+
+    @Test
+    fun searchCloseBtnShouldNotExistOnMainPage() {
+        actions(xpath("//*[contains(@text, 'Search Wikipedia')]"), WebElement::click, "Can't find Search Wikipedia input")
+        val searchCloseBtn = actions(id("search_close_btn"), WebElement::click, "Can't find Search Close Bottom")
+        assertThat(searchCloseBtn, should(not(exists())))
     }
 
     /**
-     * Checking that an element is present on the DOM of a page and then make actions with it
+     * Checking that an webelement is present on the DOM of a page and then make actions with it
      */
     private fun actions(by: By, function: (WebElement) -> Unit, errorMassage: String = "Can't find element", timeOut: Long = 5): WebElement {
         val driverWait = WebDriverWait(driver, timeOut).withMessage("$errorMassage\n")
@@ -58,6 +69,13 @@ class FirstTest {
         return element
     }
 
+    /**
+     * Check if webelement is present or not
+     */
+    private fun waitForElementNotPresent(id:String, errorMessage:String, timeOut: Long):Boolean {
+        val driverWait = WebDriverWait(driver, timeOut).withMessage("$errorMessage\n")
+        return driverWait.until(ExpectedConditions.invisibilityOfElementLocated(By.id(id)))
+    }
 
     @After
     fun tearDown() {
@@ -66,4 +84,3 @@ class FirstTest {
         }
     }
 }
-
