72bc1bfb01be3fe8c5dca4cf56b4862ecab34a5b
artsok <relix@mail.ru>
2018-07-23 17:13:51 +0300

    add lesson "10. Assertions"

--- avt-mob-3/src/test/kotlin/io/github/artsok/training/FirstTest.kt
+++ avt-mob-3/src/test/kotlin/io/github/artsok/training/FirstTest.kt
@@ -4,6 +4,7 @@ package io.github.artsok.training
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
@@ -45,8 +46,11 @@ class FirstTest {
 
     @Test
     fun shouldFindSpecialWordInSearchResultList() {
-        actions(xpath("//*[contains(@text, 'Search Wikipedia')]"), WebElement::click, "Can't find Search Wikipedia input")
-        actions(xpath("//*[contains(@text, 'Search…')]"), { element: WebElement -> element.sendKeys("Java") })
+        actions(xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                WebElement::click,
+                "Can't find Search Wikipedia input")
+        actions(xpath("//*[contains(@text, 'Search…')]"),
+                { element: WebElement -> element.sendKeys("Java") })
         assertThat(driver, decorateMatcherWithWaiter(
                 canFindElement(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
                         "//*[contains(@text, 'Object-oriented programming language')]")), timeoutHasExpired(5000L)))
@@ -54,20 +58,42 @@ class FirstTest {
 
     @Test
     fun searchCloseBtnShouldNotExistOnMainPage() {
-        actions(xpath("//*[contains(@text, 'Search Wikipedia')]"), WebElement::click, "Can't find Search Wikipedia input")
-        actions(xpath("//*[contains(@text, 'Search…')]"), { element: WebElement -> element.sendKeys("Allure") })
-        actions(id("org.wikipedia:id/search_src_text"), { element: WebElement -> element.clear() })
-        val searchCloseBtn = actions(id("search_close_btn"), WebElement::click, "Can't find Search Close Bottom")
+        actions(xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                WebElement::click,
+                "Can't find Search Wikipedia input")
+        actions(xpath("//*[contains(@text, 'Search…')]"),
+                { element: WebElement -> element.sendKeys("Allure") })
+        actions(id("org.wikipedia:id/search_src_text"),
+                { element: WebElement -> element.clear() })
+        val searchCloseBtn = actions(id("search_close_btn"),
+                WebElement::click,
+                "Can't find Search Close Bottom")
         assertThat(searchCloseBtn, should(not(exists())))
+    }
+
+    @Test
+    fun articleShouldHaveSpecialTitle() {
+        actions(xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                WebElement::click,
+                "Can't find Search Wikipedia input")
+        actions(xpath("//*[contains(@text, 'Search…')]"),
+                { element: WebElement -> element.sendKeys("Java") })
+        actions(xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text, 'Object-oriented programming language')]"),
+                WebElement::click,
+                "Can't find element with text 'Object-oriented programming language'")
+        val titleElement = actions(id("org.wikipedia:id/view_page_title_text"))
+        val articleTitle = titleElement.getAttribute("text")
+        assertThat(articleTitle, equalTo("Java (programming language)"))
     }
 
     /**
      * Checking that an webelement is present on the DOM of a page and then make actions with it
      */
-    private fun actions(by: By, function: (WebElement) -> Unit, errorMassage: String = "Can't find element", timeOut: Long = 5): WebElement {
+    private fun actions(by: By, function: ((WebElement) -> Unit)? = null, errorMassage: String = "Can't find element '$by'", timeOut: Long = 5): WebElement {
         val driverWait = WebDriverWait(driver, timeOut).withMessage("$errorMassage\n")
         val element = driverWait.until(ExpectedConditions.presenceOfElementLocated(by))
-        function.invoke(element)
+        val action = function ?: return element
+        action.invoke(element)
         return element
     }
 
