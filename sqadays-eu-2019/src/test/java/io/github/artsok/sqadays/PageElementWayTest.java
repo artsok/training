d6107034f3228a2fe640c0302229835a60597fa6
artsok <relix@mail.ru>
2019-03-12 15:12:12 +0300

    add old way - page element pattern based on classes

--- sqadays-eu-2019/src/test/java/io/github/artsok/sqadays/PageElementWayTest.java
+++ sqadays-eu-2019/src/test/java/io/github/artsok/sqadays/PageElementWayTest.java
@@ -1,48 +1,25 @@
 package io.github.artsok.sqadays;
 
 import io.github.artsok.sqadays.pages.MainPage;
-
-
 import io.github.artsok.sqadays.pages.SearchPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-//@ExtendWith(SeleniumExtension.class)
+@ExtendWith(SeleniumExtension.class)
 public class PageElementWayTest {
 
-    WebDriver driver;
-//
-//    public PageElementWayTest(ChromeDriver driver) {
-//        this.driver = driver;
-//    }
-
-    @BeforeEach
-    void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-    }
-
     @Test
-    void simpleTestWithPageElementPattern()  {
+    void simpleTestWithPageElementPattern(ChromeDriver driver)  {
         MainPage mainPage = new MainPage(driver);
         mainPage.openPage().getHeader().getSearch().sendKeys("Atlas");
         mainPage.getHeader().getSearch().submit();
         SearchPage searchPage = new SearchPage(driver);
         assertThat(searchPage.getRepositories(), hasSize(10));
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.close();
     }
 
 }
