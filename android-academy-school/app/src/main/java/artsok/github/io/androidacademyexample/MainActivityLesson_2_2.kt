b1162ec6d8e735c2ba229b2795b20f788698ee26
artsok <relix@mail.ru>
2019-04-15 16:40:00 +0300

    add homework#2.2 Using Constraint Layout. Add new layout

---
+++ android-academy-school/app/src/main/java/artsok/github/io/androidacademyexample/MainActivityLesson_2_2.kt
@@ -0,0 +1,14 @@
+package artsok.github.io.androidacademyexample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class MainActivityLesson_2_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lesson_2_2)
+
+    }
+}
+
